db67331a60400ef9773e2f55d3f9a8b80445f2d8
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-15 21:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+// Function to remove articles for sorting
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Sort ignoring "a", "an", "the"
+const sortedBands = bands.sort((a, b) =>
+  stripArticle(a).localeCompare(stripArticle(b))
+);
+
+// Display on page
+const ul = document.getElementById('band-list');
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
